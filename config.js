3ce63f694153646c7d914209742766bf81224c30
WebKarya <146603597+WebKarya@users.noreply.github.com>
2023-12-06 19:52:30 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+//Config.js berguna untuk mengatur seluruh pengaturan website.
+//Jangan mengubah config.js jika tidak tau fungsinya.
+
+const webTitle = "CiptaHost"; //untuk judul website
+const authorWeb = "CiptaHost"; //author website jangan diubah
+
+document.getElementById('webTitle').innerText = webTitle;
